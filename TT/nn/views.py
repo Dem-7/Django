5df76de46f9d040f24a36a3e61ac251d0c347a0e
Dem-7 <dem67713@gmail.com>
2025-03-13 20:13:24 +0300

    new

---
+++ TT/nn/views.py
@@ -0,0 +1,8 @@
+
+from django.http import HttpResponse
+
+def ddt(request):
+    return HttpResponse("<h1> Это первая страница моего сайта на Django </h1>")
+
+def ddd(request):
+    return HttpResponse("<h1> Это вторая страница моего сайта на Django </h1>")
